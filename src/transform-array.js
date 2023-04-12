0f0e383cea26fe6ea27970089a8d7e070e85cc9c
Vladimir Myazin <v.myazin@gmail.com>
2023-04-12 17:28:46 +0400

    feat: make some tasks Wed, Apr 12, 2023  5:28:46 PM

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,9 +13,35 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform(arr) {
+  let result = [];
+  if(!Array.isArray(arr)) {throw new Error("'arr' parameter must be an instance of the Array!");}
+  for (let i = 0; i < arr.length; i++) {
+    switch(arr[i]){
+      case '--discard-next':
+        i++
+        break;
+      case '--discard-prev':
+        if ((arr[i-2] != '--discard-next') && (arr[i-2])){
+          result.pop();
+        }        
+        break;
+      case '--double-next':
+        if(arr[i+1]){
+          result.push(arr[i+1]);
+        }        
+        break;
+      case '--double-prev':
+        if ((arr[i-2]!= '--discard-next') && (arr[i-2])) {
+          result.push(arr[i-1]);
+        }
+        break;
+      default:
+        result.push(arr[i]);        
+        break;
+    }
+  };
+  return result;
 }
 
 module.exports = {
--- src/what-season.js
+++ src/what-season.js
@@ -11,9 +11,21 @@ const { NotImplementedError } = require('../extensions/index.js');
  * getSeason(new Date(2020, 02, 31)) => 'spring'
  * 
  */
-function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getSeason(date) {
+  if(!date) {return 'Unable to determine the time of year!';}
+  //if(!date.hasOwnProperty('setDate')) {throw new Error('Invalid date!');}
+  let result = '';  
+  let month;
+  try {
+    month = date.toJSON();
+    month = date.getMonth();
+    if(month === 11 || month === 0 || month === 1) {result = 'winter';}
+    if(month === 2 || month === 3 || month === 4) {result = 'spring';}
+    if(month === 5 || month ===6 || month === 7) {result = 'summer';}
+    if(month === 8 || month === 9 || month === 10) {result = 'autumn';}
+  }
+  catch(err) {throw new Error('Invalid date!');}
+  return result;
 }
 
 module.exports = {
